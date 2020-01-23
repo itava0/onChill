50224e26319e42738b1a2d9af89477c90f9c6ea9
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-01-23 18:23:21 -0500

    Create a new component called dashboard to display more in detail the information of the movies the user clicked

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {movieUrl, key} from "../utils/movieService";
-import {Fade} from "react-reveal";
-import {ClipLoader} from "react-spinners";
+import { movieUrl, key } from "../utils/movieService";
+import { Fade } from "react-reveal";
+import { ClipLoader } from "react-spinners";
 import StarRatingComponent from "react-star-rating-component";
 
-const Dashboard = () => {
-  cosnt [movie, setMovie] = useState({});
+const Dashboard = props => {
+  const [movie, setMovie] = useState({});
   const [trailer, setTrailer] = useState({});
   const [genres, setGenres] = useState([]);
-  const [rating , setRating] = useState(null);
+  const [rating, setRating] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const queryString = `${movieUrl}${props.match.params.id}?api_key=${key}`
+  // useEffect(() => {
+  //   const queryString = `${movieUrl}${props.match.params.id}?api_key=${key}`;
 
-    const {data: movieu}
-    
-  });
+  //   axios.get(queryString).then((data) =>);
+  // });
 
-  return;
-}
- 
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+};
+
 export default Dashboard;
